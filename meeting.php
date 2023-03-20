69ec18807102ff70702291ad392ff9ab5b47e9c5
elimin8er54 <shaunt.keshishian@gmail.com>
2023-03-20 13:49:51 -0400

    More meeting changes

--- meeting.php
+++ meeting.php
@@ -1,7 +1,7 @@
 <?php  
 require 'header.php';
 
-$meeting_query = "SELECT meeting_id,meeting_name,group_id,start_time,date,capacity,announcement FROM meetings a
+$meeting_query = "SELECT meeting_id,meeting_name,group_id,start_time,a.time_slot_id,date,capacity,announcement FROM meetings a
 INNER JOIN time_slot b ON a.time_slot_id = b.time_slot_id;";
 $meeting_result = mysqli_query($db, $meeting_query); 
 
@@ -29,7 +29,7 @@ $group_result = mysqli_query($db, $group_query);
             <tr bgcolor="#ffffff">
                 <td><input type="text" name="meeting_name" required></td>
                 <td><input type="date" type="text" name="date" required></td>
-                <td><input type="time" name="time" min="00:00" max="24:00" required></td>
+                <td><input type="time" name="time" min="00:00:00" max="23:59:59" required></td>
                 <td><select name="group_id" required> <?php  while ($row = $group_result->fetch_assoc()) { ?> <option
                             value="<?php echo $row['group_id']; ?>"> <?php echo $row['name']; ?>
                         </option> <?php } ?> </select></td>
@@ -58,20 +58,24 @@ $group_result = mysqli_query($db, $group_query);
                 while ($row = $meeting_result->fetch_assoc()) { ?>
             <tr bgcolor="#ffffff">
 
-                <td><input value="<?php echo $row['meeting_name'] ?>" type="text" name="meeting_name" required></td>
-                <td><input value="<?php echo $row['date'] ?>" type="date" type="text" name="date" required></td>
-                <td><input value="<?php echo $row['start_time'] ?>" type="time" name="time" min="00:00" max="24:00"
-                        required>
+                <td> <input value="<?php echo $row['meeting_id']; ?>" name="meeting_id[]" type="hidden">
+                    <input value="<?php echo $row['time_slot_id']; ?>" name="time_slot_id[]" type="hidden">
+                    <input value="<?php echo $row['meeting_name']; ?>" type="text" name="meeting_name[]" required>
                 </td>
-                <td><select name="group_id" required> <?php  while ($row2 = $group_result->fetch_assoc()) { ?> <option
+                <td><input value="<?php echo $row['date']; ?>" type="date" type="text" name="date[]" required></td>
+                <td><input value="<?php echo $row['start_time']; ?>" type="time" name="time[]" min="00:00:00"
+                        max="23:59:59" required>
+                </td>
+                <td><select name="group_id[]" required> <?php  while ($row2 = $group_result->fetch_assoc()) { ?> <option
                             <?php  if($row['group_id'] == $row2['group_id']) { echo "selected"; } ?>
                             value="<?php echo $row2['group_id']; ?>"> <?php echo $row2['name']; ?>
                         </option> <?php  } ?> </select></td>
-                <td><input value="<?php echo $row['announcement'] ?>" type="text" name="announcment"></td>
+                <td><input value="<?php echo $row['announcement']; ?>" type="text" name="announcement[]"></td>
 
             </tr>
             <?php  mysqli_data_seek($group_result,0); }?>
         </table>
+
         </br>
 
         <input type="submit" value="Update" name="update" />

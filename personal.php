eb524d8197a2b8264220e19e6828d45ea753cd55
Jordan Gyaltsen <jordan-gyaltsen@users.noreply.github.com>
2023-03-28 21:46:20 -0700

    Add files via upload

--- personal.php
+++ personal.php
@@ -1,8 +1,6 @@
 <?php  
 //check to see that userID properly set
-
 //by default getting an error saying user does not exist. Commented out to see errors.
-
 require 'header.php';
 if(!isset($_GET['user_id'])) {
     echo "This user does not exist";
@@ -19,6 +17,7 @@ if($personal_usertype == "student")  {
                         WHERE `id`='$personal_userid';";
     $userpersonal_result = mysqli_query($db, $userpersonal_query); 
 
+// if user is parent, query there name
 } else if ($personal_usertype == "parent") {
     $userpersonal_query = "SELECT name FROM users 
     WHERE `id`='$personal_userid';";
@@ -27,7 +26,7 @@ if($personal_usertype == "student")  {
 
 //If user is a parent, query for all their children
 if($user_type == "parent") {
-    $c_query = "SELECT name FROM users
+    $c_query = "SELECT name,id FROM users
     INNER JOIN students as b ON id = b.student_id
     INNER JOIN child_of as a ON a.student_id = b.student_id
     WHERE `parent_id`='$user_id';";
@@ -43,20 +42,23 @@ $personal_allergies = "";
 
 while ($row = $userpersonal_result->fetch_assoc()) {
     $personal_name = $row['name'];
-    $personal_bday = $row['bday'];
-    $personal_pronouns = $row['pronouns'];
-    $personal_allergies = $row['allergies'];
+    if($personal_usertype == "student") {
+        $personal_bday = $row['bday'];
+        $personal_pronouns = $row['pronouns'];
+        $personal_allergies = $row['allergies'];
+    }
 }
 ?>
 
 </br>
+
 <div>
+<?php if($personal_usertype == "student") { ?>
     <form action="personal_script.php" method="post">
         <table>
             <tr>
                 <td><?php echo $personal_name; ?></td>
             </tr>
-        <?php if($personal_usertype == "student") { ?>
             <tr>
                 <td>Birthday</td>
                 <td><input value="<?php echo $personal_bday; ?>" type="date" name="bday" id="bday" placeholder="" required /></td>
@@ -64,7 +66,7 @@ while ($row = $userpersonal_result->fetch_assoc()) {
             <tr>
                 <td>Pronouns</td>
                 <td>
-                    <input value="<?php echo $personal_allergies; ?>" type="text" name="pronouns"
+                    <input value="<?php echo $personal_pronouns; ?>" type="text" name="pronouns"
                         placeholder="Fill in your desired pronouns" id="student pronouns">
                 </td>
             </tr>
@@ -76,23 +78,23 @@ while ($row = $userpersonal_result->fetch_assoc()) {
                 </td>
             </tr>
 
-        <?php } ?>
         </table>
         <input value="<?php echo $personal_usertype; ?>" name="t" type="hidden">
         <input value="<?php echo $personal_userid; ?>" name="i" type="hidden">
         <input type="submit" value="Update" name="submit" />
     </form>
+    <?php } ?>
 
     <?php if($user_type == "parent") { ?>
-    <p>My Children</p>
-    <?php while ($row = $c_result->fetch_assoc()) { ?>
-        <a href="settings.php?user_id=<?php echo $row['id']; ?>&user_type=student"><?php echo $row['name']; ?></a><br>
-    <?php
-    }
-    }
-    ?>
+        <p>My Children</p>
+        <?php  
+            while ($row = $c_result->fetch_assoc()) { 
+        ?>
+        <a href="personal.php?user_id=<?php echo $row['id']; ?>&user_type=student"><?php echo $row['name']; ?></a><br>
+        <?php 
+        } 
+    } ?>
 </div>
-
 <?php
 require 'footer.php';
 ?>
